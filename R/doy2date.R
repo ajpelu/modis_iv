f089d4d9f4086bfe85d05e4910b3c34a3d42fd74
ajpelu <ajpelu@gmail.com>
2016-09-06 17:58:25 +0200

    update function

--- R/doy2date.R
+++ R/doy2date.R
@@ -1,6 +1,5 @@
 # Convert day of year into date format 
 # Function from Verbesselt et. al 2016 (Remote Sensing for Ecologist)
 doy2date <- function(year, doy){ 
-  d <- as.Date(doy - 1, origin = paste0(year, "-01-01"))
-  d 
-  }  
+  as.Date(doy - 1, origin = paste0(year, "-01-01"))
+}  

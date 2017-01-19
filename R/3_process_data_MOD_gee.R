205c6f552685adbd236c82e10aa315f907002ef7
Antonio <ajpelu@gmail.com>
2017-01-19 12:36:46 +0100

    add script to process data modis from GEE

---
+++ R/3_process_data_MOD_gee.R
@@ -0,0 +1,164 @@
+# --- 
+# Script to process downloaded MOD13Q1 data for Q. pyrenaica forests of Sierra Nevada # 
+# Author: Perez-Luque AJ (@ajpelu)
+# version: 1
+# date: 2017 January 
+# --- 
+
+# --- 
+# Set directory 
+machine <- 'ajpelu'
+di <- paste('/Users/', machine, '/Dropbox/phd/phd_repos/modis_iv', sep='')
+# --- 
+
+# --- 
+library(stringr)
+library(dplyr)
+library(reshape2)
+library(lubridate)
+# ---
+
+
+# Read data 
+raw <- read.csv(paste0(di, '/data_raw/modis_gee/iv_qp_raw.csv'), header=TRUE) 
+
+# Change variable names 
+
+raw <- raw %>% 
+  mutate(# Get date 
+    aux_date = stringr::str_replace(system.index, 
+                                         pattern = "MOD13Q1_005_", 
+                                         ""),
+    # Store as date 
+    date = as.Date(substr(aux_date,1,10), format = "%Y_%m_%d"),
+    # Get gee index 
+    gee_index = as.numeric(
+      stringr::str_replace(
+        substr(aux_date,11, nchar(aux_date)), pattern = '_', "")))
+
+
+
+
+
+
+
+
+test <- 'MOD13Q1_005_2000_02_18_1812'         
+         
+aux_date <- stringr::str_replace(test, pattern = "MOD13Q1_005_", "")
+         
+test_2 <- stringr::str_split(test_1, pattern = '_')[[1]][1]
+
+
+
+
+
+## read in date/time info in format 'm/d/y'
+dates <- c("02/27/92", "02/27/92", "01/14/92", "02/28/92", "02/01/92")
+as.Date(dates, "%m/%d/%y")
+
+       
+       
+       
+       year = stringr::str_split(aux_date, pattern = '_')[[1]][1], # year
+       year = stringr::str_split(aux_date, pattern = '_')[[1]][2], # month 
+       year = stringr::str_split(aux_date, pattern = '_')[[1]][3], # day
+       year = as.numeric(stringr::str_split(aux_date, pattern = '_')[[1]][4]), # gee index? 
+
+
+xx <- 
+       
+         
+         
+         
+         ## read in date/time info in format 'm/d/y'
+         dates <- c("02/27/92", "02/27/92", "01/14/92", "02/28/92", "02/01/92")
+       as.Date(dates, "%m/%d/%y")
+         
+         
+         
+         
+# --- 
+# LOOP TO PROCESS DATA 
+for (i in modis_bands){ 
+  
+  ### Get name of the band 
+  name_band <- tolower(str_replace(i, pattern = "250m_16_days_", ""))
+  
+  ### Create a MODIS_Temporal_Series
+  mimodis_ts <- MODISTimeSeries(Dir = path_original_MODIS, 
+                                Band = i, 
+                                Simplify = FALSE) 
+  
+  # Create a empty data.frame
+  aux_data_out <- c() 
+  
+  for (j in 1:length(mimodis_ts)) { 
+    
+    ### Get id of PIXEL and coordinates 
+    miname <- colnames(mimodis_ts[[j]])
+    ### Remove "Samp1Line1_pixel1" 
+    aux_miname <- str_replace(miname, pattern = "\\Samp..*", "")
+    
+    ### milat 
+    aux_miname_lat <- str_replace(aux_miname, pattern = "\\Lon..*", "")
+    milat <- as.numeric(unlist(stringr::str_split(aux_miname_lat, "Lat", n=2))[2])
+    
+    ### milong
+    milong <- as.numeric(unlist(stringr::str_split(aux_miname, "Lon", n=2))[2])
+    
+    ### Coordinates
+    mis_coord <- as.data.frame(cbind(milat, milong))
+    
+    # Get id of pixel 
+    myid <- mis_coord %>% 
+      inner_join(mydf, by= c('milat' = 'lat', 'milong' = 'long'))
+    
+    # Change name "Lat37.1964115002029Lon-3.2653977048134Samp1Line1_pixel1" for 
+    # pixel ID
+    colnames(mimodis_ts[[j]]) <- myid$ID
+    
+    aux_data <- mimodis_ts[[j]] %>% 
+      melt() %>% 
+      select('modis_date' = Var1,
+             'id_pixel' = Var2,
+             name_band = value)
+    
+    aux_data_out <- rbind(aux_data_out, aux_data)
+    
+  }
+  
+  names(aux_data_out)[3] <- name_band
+  assign(name_band, aux_data_out)
+  
+} 
+
+# --- 
+# Merge dataframes 
+iv_df <- ndvi %>% 
+  inner_join(evi, by=c("modis_date", "id_pixel")) %>% 
+  inner_join(pixel_reliability, by=c("modis_date", "id_pixel")) %>% 
+  inner_join(vi_quality, by=c("modis_date", "id_pixel")) %>% 
+  inner_join(composite_day_of_the_year, by=c("modis_date", "id_pixel")) 
+# --- 
+
+
+# ---
+# Function from Verbesselt et. al 2016 (Remote Sensing for Ecologist)
+doy2date <- function(year, doy){ 
+  as.Date(doy - 1, origin = paste0(year, "-01-01"))}  
+
+
+# Get year from modis_date and stored as variable 
+iv_df$year <- str_sub(iv_df$modis_date, start = 2, end = -4)
+
+iv_df$date <- doy2date(iv_df$year, iv_df$composite_day_of_the_year)
+# See Testa et al. 2014 http://server-geolab.agr.unifi.it/public/completed/2014_EuJRS_47_285_305_Testa.pdf
+# --- 
+
+
+# ---
+# Export evi dataframe
+write.csv(iv_df, file=paste(di, "/data/iv_raw_2015.csv", sep=""), row.names = FALSE)
+# ---
+
